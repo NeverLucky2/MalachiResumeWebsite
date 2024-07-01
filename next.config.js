2c817a23950b63c8d772c47b7c64e8d04858222d
NeverLucky2 <mtzuoo@gmail.com>
2024-06-30 21:57:56 -0500

    added export to config

--- next.config.js
+++ next.config.js
@@ -2,9 +2,10 @@
 
 // https://github.com/vercel/next.js/blob/master/packages/next/next-server/server/config.ts
 const nextConfig = {
+  output: 'export',
   webpack: config => {
     const oneOfRule = config.module.rules.find(rule => rule.oneOf);
-
+    
     // Next 12 has multiple TS loaders, and we need to update all of them.
     const tsRules = oneOfRule.oneOf.filter(rule => rule.test && rule.test.toString().includes('tsx|ts'));
 
