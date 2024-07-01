a1fd69785026e6709bc54cc086e0cf24f1008e26
NeverLucky2 <mtzuoo@gmail.com>
2024-06-30 21:58:26 -0500

    removed export

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 
 // https://github.com/vercel/next.js/blob/master/packages/next/next-server/server/config.ts
 const nextConfig = {
-  output: 'export',
   webpack: config => {
     const oneOfRule = config.module.rules.find(rule => rule.oneOf);
     
